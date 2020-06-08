78e68fe6c54d8f05152f3d3c23e6b5197142a259
Jaskarn Mankoo <jaskarn_11@hotmail.com>
2020-06-08 14:21:35 -0400

    final changes

--- Paint/src/ca/utoronto/utm/paint/Command.java
+++
@@ -1,6 +0,0 @@
-package ca.utoronto.utm.paint;
-
-
-public interface Command {
-	public abstract void execute(String ins,Point p);
-}
--- Paint/src/ca/utoronto/utm/paint/Squiggle.java
+++
@@ -1,8 +0,0 @@
-package ca.utoronto.utm.paint;
-
-public class Squiggle extends Line{
-	public Squiggle(Point p1){
-		super(p1);
-	}
-
-}
